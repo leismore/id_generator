052eb36715e139831f5e9cfda4ef2c813bcc931a
Kyle Chine <kyle@leismore.co>
2019-12-04 04:54:12 +1100

    config.json and orgID_to_couchdbID function

---
+++ src/lib/orgID_to_couchdbID.ts
@@ -0,0 +1,17 @@
+/**
+ * orgID_to_couchdbID function: From orgID_to_couchdbID view (DB: myorg).
+ */
+
+import { MyOrg }     from './type/db_doc_myorg';
+import * as NANO     from 'nano';
+import * as CONFIG   from '../config.json';
+const DESIGN_NAME    = CONFIG.couchdb.designName;
+const VIEW_NAME      = 'orgID_to_couchdbID';
+
+async function orgID_to_couchdbID(db:NANO.DocumentScope<MyOrg>, orgID:string)
+  :Promise<NANO.DocumentViewResponse<string,MyOrg>>
+{
+  return db.view(DESIGN_NAME, VIEW_NAME, {key: orgID});
+}
+
+export { orgID_to_couchdbID };
