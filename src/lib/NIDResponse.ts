eb78b84ae0e9f28642535ba172eec16f6e5c88a4
Kyle Chine <kyle@leismore.co>
2019-12-04 13:34:04 +1100

    NIDResponse done

---
+++ src/lib/NIDResponse.ts
@@ -0,0 +1,21 @@
+/**
+ * NIDResponse class for the Numeric ID API.
+ * Refer to @leismore/response <https://www.npmjs.com/package/@leismore/response>
+ */
+
+import { Response } from '@leismore/response';
+
+class NIDResponse extends Response
+{
+  public res200(id:string):void
+  {
+    this.send({
+      statusCode: '200',
+      headers: { 'Content-Type'  : 'application/json',
+                 'Cache-Control' : 'no-store' },
+      body: {'id': id}
+    });
+  }
+}
+
+export { NIDResponse };
