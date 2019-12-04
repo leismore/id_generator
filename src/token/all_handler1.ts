36c5f88c20beee7e44bef3fbdac2ef71151b156c
Kyle Chine <kyle@leismore.co>
2019-12-04 21:28:32 +1100

    token: all_handler done

---
+++ src/token/all_handler1.ts
@@ -0,0 +1,26 @@
+/**
+ * All Handler 1 - Prevent not-allowed HTTP methods
+ */
+
+import * as express    from 'express';
+import { TokenError }  from '../lib/TokenError';
+const ALLOWED        = ['OPTIONS', 'GET'];
+
+function all_handler1(req:express.Request, _res:express.Response, next:express.NextFunction):void
+{
+  if (ALLOWED.includes( req.method.toUpperCase() ) === false)
+  {
+    let error = {message: 'HTTP 405: Method Not Allowed', code: '3'};
+    let response = {
+      statusCode: '405',
+      headers: { 'Allow': ALLOWED.join(', ') }
+    };
+    next( new TokenError(error, response) );
+  }
+  else
+  {
+    next('route');
+  }
+}
+
+export { all_handler1 };
