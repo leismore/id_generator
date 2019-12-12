cb07e8e9a7a165bdc64ec8f5f0537146a48c5412
Kyle Chine <kyle@leismore.co>
2019-12-13 09:13:06 +1100

    Token API done

---
+++ src/token/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Token API
+ */
+
+import { all_handler1 } from './all_handler1';
+import { get_handler1 } from './get_handler1';
+import { get_handler2 } from './get_handler2';
+
+export { all_handler1, get_handler1, get_handler2 };
