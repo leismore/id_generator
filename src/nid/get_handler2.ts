97d70dc3dea196496df43f7021586aba63bc551d
Kyle Chine <kyle@leismore.co>
2019-12-13 16:28:25 +1100

    NID API

---
+++ src/nid/get_handler2.ts
@@ -0,0 +1,44 @@
+/**
+ * GET Handler 2 - Send new numeric ID
+ */
+
+import * as express      from 'express';
+import * as nano         from 'nano';
+import { NIDError }      from '../lib/NIDError';
+import { NIDResponse }   from '../lib/NIDResponse';
+import { connect_db }    from '../lib/connect_db';
+import { get_numericID } from '../lib/get_numericID';
+import { MyOrg }         from '../lib/type/db_doc_myorg';
+const DB_NAME            = 'myorg';
+
+function get_handler2(_req:express.Request, res:express.Response, next:express.NextFunction):void
+{
+  let resp = new NIDResponse(res);
+  let db:nano.DocumentScope<MyOrg>;
+
+  // Connect DB
+  try
+  {
+    db = connect_db().use(DB_NAME);
+  }
+  catch(e)
+  {
+    let error = {message: 'CouchDB: connection failure', code: '2'};
+    let response = {statusCode: '500'};
+    next( new NIDError(error, response, e) );
+    return;
+  }
+
+  // Get ID
+  get_numericID(db).then(r => {
+    resp.res200(r.status);
+    return;
+  }).catch(e => {
+    let error = {message: 'get_numericID failure', code: '6'};
+    let response = {statusCode: '500'};
+    next( new NIDError(error, response, e) );
+    return;
+  });
+}
+
+export { get_handler2 };
