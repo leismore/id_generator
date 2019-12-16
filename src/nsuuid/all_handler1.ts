75ca602f7227f86f88ad7098857f71f34a745af2
Kyle Chine <kyle@leismore.co>
2019-12-16 11:37:02 +1100

    NSUUID: all_handler1

---
+++ src/nsuuid/all_handler1.ts
@@ -0,0 +1,28 @@
+/**
+ * All Handler 1 - Prevent not-allowed HTTP methods
+ */
+
+import * as express     from 'express';
+import { NSUUIDError }  from '../lib/NSUUIDError';
+const ALLOWED        = ['OPTIONS', 'GET'];
+
+function all_handler1(req:express.Request, _res:express.Response, next:express.NextFunction):void
+{
+  if (ALLOWED.includes( req.method.toUpperCase() ) === false)
+  {
+    let error = {message: 'HTTP 405: Method Not Allowed', code: '3'};
+    let response = {
+      statusCode: '405',
+      headers: { 'Allow': ALLOWED.join(', ') }
+    };
+    next( new NSUUIDError(error, response) );
+    return;
+  }
+  else
+  {
+    next('route');
+    return;
+  }
+}
+
+export { all_handler1 };
