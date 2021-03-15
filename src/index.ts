e0da65cfb8c82881d0601a376ee65d0850a1a062
Kyle Chine <kyle.chine@leismore.co>
2021-03-15 16:53:46 +1100

    Allowing HEAD HTTP request for testing

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@
 import * as express           from 'express';
 import * as cors              from 'cors';
 import { corsOrigin }         from './corsOrigin';
+import { get_handler as head_handler } from '@leismore/get_handler';
 import { error_handler_last } from '@leismore/error_handler_last';
 import * as token             from './token/index';
 import * as nid               from './nid/index';
@@ -16,7 +17,7 @@ import * as CONFIG            from './config.json';
 
 const corsOptions:cors.CorsOptions = {
   origin:  corsOrigin,
-  methods: ['OPTIONS', 'GET']
+  methods: ['OPTIONS', 'HEAD', 'GET']
 };
 
 // Init.
@@ -27,30 +28,35 @@ app.use( cors(corsOptions) );
 const        TOKEN_URL = CONFIG.api.baseURL + CONFIG.api.token.url;
 app.all(     TOKEN_URL, token.all_handler1 );
 app.options( TOKEN_URL, ()=>{} );
+app.head(    TOKEN_URL, head_handler );
 app.get(     TOKEN_URL, token.get_handler1, token.get_handler2 );
 
 // 2. NID (Numeric ID)
 const        NID_URL = CONFIG.api.baseURL + CONFIG.api.numeric.url;
 app.all(     NID_URL, nid.all_handler1 );
 app.options( NID_URL, ()=>{} );
+app.head(    NID_URL, head_handler );
 app.get(     NID_URL, nid.get_handler1, nid.get_handler2 );
 
 // 3. SID (Short ID)
 const        SID_URL = CONFIG.api.baseURL + CONFIG.api.short.url;
 app.all(     SID_URL, sid.all_handler1 );
 app.options( SID_URL, ()=>{} );
+app.head(    SID_URL, head_handler );
 app.get(     SID_URL, sid.get_handler1, sid.get_handler2 );
 
 // 4. UUID (Random)
 const        RUUID_URL = CONFIG.api.baseURL + CONFIG.api.uuidRand.url;
 app.all(     RUUID_URL, ruuid.all_handler1 );
 app.options( RUUID_URL, ()=>{} );
+app.head(    RUUID_URL, head_handler );
 app.get(     RUUID_URL, ruuid.get_handler1, ruuid.get_handler2 );
 
 // 5. UUID (Namespace)
 const        NSUUID_URL = CONFIG.api.baseURL + CONFIG.api.uuidNS.url;
 app.all(     NSUUID_URL, nsuuid.all_handler1 );
 app.options( NSUUID_URL, ()=>{} );
+app.head(    NSUUID_URL, head_handler );
 app.get(     NSUUID_URL, nsuuid.get_handler1, nsuuid.get_handler2, nsuuid.get_handler3 );
 
 // Error handling
--- src/nsuuid/all_handler1.ts
+++ src/nsuuid/all_handler1.ts
@@ -6,7 +6,7 @@ import {
     all_handler_generator            as generator,
     all_handler_LMErrorRes_generator as gen_response } from '@leismore/all_handler';
 import { NSUUIDError }             from '../lib/NSUUIDError';
-const ALLOWED      = ['OPTIONS', 'GET'];
+const ALLOWED      = ['OPTIONS', 'HEAD', 'GET'];
 const ERROR        = new NSUUIDError({message: 'HTTP 405: Method Not Allowed', code: '3'}, gen_response(ALLOWED));
 const all_handler1 = generator(ALLOWED, ERROR);
 
