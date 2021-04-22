90a6a298b749e60de82588d2823940a13cfc6e1c
Kyle Chine <kyle.chine@leismore.co>
2021-04-22 21:45:07 +1000

    index.ts: Global CORS has been removed.

--- src/index.ts
+++ src/index.ts
@@ -12,8 +12,6 @@
 
 // Import modules
 import * as express           from 'express';
-import * as cors              from 'cors';
-import { corsOrigin }         from './corsOrigin';
 import { get_handler as head_handler } from '@leismore/get_handler';
 import { error_handler_last } from '@leismore/error_handler_last';
 import * as init              from './init/index';
@@ -24,17 +22,13 @@ import * as ruuid             from './ruuid/index';
 import * as nsuuid            from './nsuuid/index';
 import * as CONFIG            from './config.json';
 
-const corsOptions:cors.CorsOptions = {
-  origin:  corsOrigin,
-  methods: ['OPTIONS', 'HEAD', 'GET']
-};
-
 // Init.
 let app = express();
-app.use( init.init_handler1, cors(corsOptions) );
+app.use( init.init_handler1 );
 
 // 1. Token
 const        TOKEN_URL = CONFIG.api.baseURL + CONFIG.api.token.url;
+app.use(     TOKEN_URL, token.cors_handler );
 app.all(     TOKEN_URL, token.all_handler1 );
 app.options( TOKEN_URL, ()=>{} );
 app.head(    TOKEN_URL, head_handler );
@@ -42,6 +36,7 @@ app.get(     TOKEN_URL, token.get_handler1, token.get_handler2 );
 
 // 2. NID (Numeric ID)
 const        NID_URL = CONFIG.api.baseURL + CONFIG.api.numeric.url;
+app.use(     NID_URL, nid.cors_handler );
 app.all(     NID_URL, nid.all_handler1 );
 app.options( NID_URL, ()=>{} );
 app.head(    NID_URL, head_handler );
@@ -49,6 +44,7 @@ app.get(     NID_URL, nid.get_handler1, nid.get_handler2 );
 
 // 3. SID (Short ID)
 const        SID_URL = CONFIG.api.baseURL + CONFIG.api.short.url;
+app.use(     SID_URL, sid.cors_handler );
 app.all(     SID_URL, sid.all_handler1 );
 app.options( SID_URL, ()=>{} );
 app.head(    SID_URL, head_handler );
@@ -56,6 +52,7 @@ app.get(     SID_URL, sid.get_handler1, sid.get_handler2 );
 
 // 4. UUID (Random)
 const        RUUID_URL = CONFIG.api.baseURL + CONFIG.api.uuidRand.url;
+app.use(     RUUID_URL, ruuid.cors_handler );
 app.all(     RUUID_URL, ruuid.all_handler1 );
 app.options( RUUID_URL, ()=>{} );
 app.head(    RUUID_URL, head_handler );
@@ -63,6 +60,7 @@ app.get(     RUUID_URL, ruuid.get_handler1, ruuid.get_handler2 );
 
 // 5. UUID (Namespace)
 const        NSUUID_URL = CONFIG.api.baseURL + CONFIG.api.uuidNS.url;
+app.use(     NSUUID_URL, nsuuid.cors_handler );
 app.all(     NSUUID_URL, nsuuid.all_handler1 );
 app.options( NSUUID_URL, ()=>{} );
 app.head(    NSUUID_URL, head_handler );
