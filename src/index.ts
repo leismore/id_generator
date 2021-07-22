dfd10b82249de497b60068a78a0c854968efcd3c
Kyle Chine <kyle.chine@leismore.co>
2021-07-22 10:00:52 +1000

    src done, updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,7 @@ import * as nid               from './nid/index';
 import * as nidOrg            from './nid_org/index';
 import * as nidUser           from './nid_user/index';
 import * as sid               from './sid/index';
+import * as serid             from './serid/index';
 import * as ruuid             from './ruuid/index';
 import * as nsuuid            from './nsuuid/index';
 import * as nsuuidOrg         from './nsuuid_org/index';
@@ -77,21 +78,28 @@ app.all(     SID_URL, sid.all_handler1 );
 app.head(    SID_URL, head_handler );
 app.get(     SID_URL, sid.get_handler1, sid.get_handler2 );
 
-// 6. UUID (Random)
+// 6. SERID (Shorter ID)
+const        SERID_URL = CONFIG.api.baseURL + CONFIG.api.shorter.url;
+app.use(     SERID_URL, serid.cors_handler );
+app.all(     SERID_URL, serid.all_handler1 );
+app.head(    SERID_URL, head_handler );
+app.get(     SERID_URL, serid.get_handler1, serid.get_handler2 );
+
+// 7. UUID (Random)
 const        RUUID_URL = CONFIG.api.baseURL + CONFIG.api.uuidRand.url;
 app.use(     RUUID_URL, ruuid.cors_handler );
 app.all(     RUUID_URL, ruuid.all_handler1 );
 app.head(    RUUID_URL, head_handler );
 app.get(     RUUID_URL, ruuid.get_handler1, ruuid.get_handler2 );
 
-// 7. UUID (Namespace)
+// 8. UUID (Namespace)
 const        NSUUID_URL = CONFIG.api.baseURL + CONFIG.api.uuidNS.url;
 app.use(     NSUUID_URL, nsuuid.cors_handler );
 app.all(     NSUUID_URL, nsuuid.all_handler1 );
 app.head(    NSUUID_URL, head_handler );
 app.get(     NSUUID_URL, nsuuid.get_handler1, nsuuid.get_handler2, nsuuid.get_handler3 );
 
-// 8. UUID (Namespace) Org
+// 9. UUID (Namespace) Org
 const        NSUUID_ORG_URL = CONFIG.api.baseURL + CONFIG.api.uuidNSOrg.url;
 app.use(     NSUUID_ORG_URL, nsuuidOrg.cors_handler );
 app.all(     NSUUID_ORG_URL, nsuuidOrg.all_handler1 );
@@ -104,7 +112,7 @@ app.post(    NSUUID_ORG_URL,
   nsuuidOrg.post_handler5
 );
 
-// 9. UUID (Namespace) User
+// 10. UUID (Namespace) User
 const        NSUUID_USER_URL = CONFIG.api.baseURL + CONFIG.api.uuidNSUser.url;
 app.use(     NSUUID_USER_URL, nsuuidUser.cors_handler );
 app.all(     NSUUID_USER_URL, nsuuidUser.all_handler1 );
